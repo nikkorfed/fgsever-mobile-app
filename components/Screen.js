fb53e64f7d19e25452b6d8721addafdf8477c8df
Никита Корнилов <nikkorfed@gmail.com>
2022-10-02 00:15:08 +0300

    Adjust header styles.

--- components/Screen.js
+++ components/Screen.js
@@ -3,21 +3,16 @@ import { StyleSheet, SafeAreaView, View, ScrollView } from "react-native";
 
 export default Screen = ({ children }) => {
   return (
-    <SafeAreaView style={styles.screen}>
-      <ScrollView style={styles.wrapper} contentInsetAdjustmentBehavior="automatic">
-        {children}
-      </ScrollView>
-    </SafeAreaView>
+    <ScrollView style={styles.wrapper} contentInsetAdjustmentBehavior="automatic">
+      {children}
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  screen: {
-    flex: 1,
-    backgroundColor: "#f8f8f8",
-  },
   wrapper: {
     flex: 1,
     paddingHorizontal: 15,
+    backgroundColor: "#f8f8f8",
   },
 });
--- navigators/Home.js
+++ navigators/Home.js
@@ -5,13 +5,14 @@ import HomeScreen from "../screens/Home";
 import MaintenanceCalculatorScreen from "../screens/MaintenanceCalculator";
 import UpgradeCalculatorScreen from "../screens/UpgradeCalculator";
 import AppointmentScreen from "../screens/Appointment";
-import { Text } from "react-native";
 
 const Stack = createNativeStackNavigator();
 
 export default HomeNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerTransparent: true }}>
+    <Stack.Navigator
+      screenOptions={{ headerTransparent: true, headerBlurEffect: "light", headerLargeStyle: { backgroundColor: "#f8f8f8" } }}
+    >
       <Stack.Screen name="Home" component={HomeScreen} options={{ headerLargeTitle: true, title: "Главная" }} />
       <Stack.Screen name="MaintenanceCalculator" component={MaintenanceCalculatorScreen} options={{ title: "Калькулятор ТО" }} />
       <Stack.Screen name="UpgradeCalculator" component={UpgradeCalculatorScreen} options={{ title: "Калькулятор дооснащения" }} />
