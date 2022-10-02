fd02f2c2e76f6f03c2a061cdb8ab587854de7777
Никита Корнилов <nikkorfed@gmail.com>
2022-10-02 23:16:54 +0300

    Change approach for scrolling to text inputs.

--- components/Screen.js
+++ components/Screen.js
@@ -1,16 +1,32 @@
 import React from "react";
-import { StyleSheet } from "react-native";
+import { StyleSheet, KeyboardAvoidingView, Platform, ScrollView } from "react-native";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
+import { useKeyboardVisible } from "../hooks/keyboard";
+
+// Текущий вид прокрутки для текстовых полей корректно работает с полосой прокрутки, но имеет скачки.
+
 const Screen = ({ children }) => {
+  const isKeyboardVisible = useKeyboardVisible();
+
   return (
-    <KeyboardAwareScrollView style={styles.wrapper} contentInsetAdjustmentBehavior="automatic">
-      {children}
-    </KeyboardAwareScrollView>
+    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <ScrollView
+        style={styles.wrapper}
+        contentInsetAdjustmentBehavior="automatic"
+        contentContainerStyle={{ paddingBottom: isKeyboardVisible ? 0 : 49 }}
+        scrollIndicatorInsets={{ bottom: isKeyboardVisible ? 0 : 49 }}
+      >
+        {children}
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   wrapper: {
     flex: 1,
     paddingHorizontal: 15,
