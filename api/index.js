d47e3c159bcf4f8de3dac2cb659c5098e9188610
Никита Корнилов <nikkorfed@gmail.com>
2022-12-13 13:02:16 +0500

    Adjust serializer in API.

--- api/index.js
+++ api/index.js
@@ -2,11 +2,18 @@ import axios from "axios";
 import { encode } from "js-base64";
 
 import { prepareWork } from "../helpers/works";
+import { worksResponse } from "../mocks";
+
+const serializer = (params) => {
+  const entries = Object.entries(params);
+  return entries.map(([key, value]) => `${key}=${value}`).join("&");
+};
 
 const api = axios.create({
   baseURL: "http://213.109.27.99:27/AutoserviceFgsever/odata/standard.odata/",
   headers: { Authorization: `Basic ${encode("КурочкинМ:789")}` },
   params: { $format: "json" },
+  paramsSerializer: { serialize: serializer },
 });
 
 const workTypes = () => api.get("/Catalog_асВидыРемонта");
