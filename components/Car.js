6d6beca4599f377a2d8ed51acd8c42eebf035733
Никита Корнилов <nikkorfed@gmail.com>
2022-12-20 14:16:00 +0500

    Add button for deleting cars from garage.

--- components/Car.js
+++ components/Car.js
@@ -1,9 +1,20 @@
+import { FontAwesome5 } from "@expo/vector-icons";
 import React from "react";
-import { StyleSheet, Image, View, Text, Dimensions } from "react-native";
+import { StyleSheet, Image, View, Text, Dimensions, Pressable, Alert } from "react-native";
 
+import { useStore } from "../hooks/store";
 import globalStyles from "../styles";
 
-const Car = ({ style, image, name, vin, model, modelCode, productionDate }) => {
+const Car = ({ style, guid, name, image, vin, model, modelCode, productionDate }) => {
+  const { setCars } = useStore();
+
+  const handleDelete = () => {
+    Alert.alert(`Вы действительно хотите удалить ${name}?`, null, [
+      { text: "Отмена" },
+      { text: "Удалить", onPress: () => setCars((prev) => prev.filter((car) => car.guid !== guid)), style: "destructive" },
+    ]);
+  };
+
   return (
     <View style={[styles.block, style]}>
       <Image style={styles.image} resizeMode="contain" source={{ uri: image }} />
@@ -26,12 +37,16 @@ const Car = ({ style, image, name, vin, model, modelCode, productionDate }) => {
         <Text style={styles.description}>Дата изготовления: </Text>
         <Text style={styles.text}>{productionDate}</Text>
       </View>
+      <Pressable style={styles.icon} onPress={handleDelete}>
+        <FontAwesome5 name="trash" color="lightgrey" size={12} />
+      </Pressable>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   block: {
+    position: "relative",
     borderWidth: 1,
     borderColor: "#eee",
     borderRadius: 10,
@@ -58,6 +73,13 @@ const styles = StyleSheet.create({
   },
   description: {
     ...globalStyles.description,
+  },
+  icon: {
+    position: "absolute",
+    top: 5,
+    right: 5,
+    borderColor: "lightgrey",
+    padding: 10,
   },
 });
 
